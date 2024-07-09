be6146454b1ceb6c3348e68fc9ae597c79b28f50
Pragadeeshwaran9334 <146710463+Pragadeeshwaran9334@users.noreply.github.com>
2024-07-09 00:34:41 -0700

    Time: 2 ms (48.07%), Space: 42.5 MB (62.26%) - LeetHub

---
+++ 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findPermutationDifference(String s, String t) {
+        int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            for(int j=0;j<t.length();j++)
+            {
+                if(s.charAt(i)==t.charAt(j))
+                {
+                    count+=Math.abs(i-j);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+           
+        
+        }
+    return count;
+}
+}
